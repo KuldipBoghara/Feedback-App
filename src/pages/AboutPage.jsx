68dc4c727177abcbfeedda4ca19c9a2c502a2978
Mr-BK <bogharakuldip@gmail.com>
2022-03-02 12:30:07 -0500

    Routes

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Card from '../components/shared/Card';
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h1> About This Project</h1>
+        <p>This is a React App to leave feedback for a service</p>
+        <p>Version: 1.0.0</p>
+
+        <p>
+          <Link to="/"> Back To Home</Link>
+        </p>
+      </div>
+    </Card>
+  );
+}
+
+export default AboutPage;
